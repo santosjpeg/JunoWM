cf8c2497920e981fc125863e611079885fea05b6
aaron <santosaaron2004@gmail.com>
2025-04-02 00:09:14 -0400

    client: bind to globals, server: registering add/remove globals

--- src-bak/client.c
+++ src-bak/client.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
 #include <wayland-client.h>
 
-int main(int argc, char **argv) {
-  struct wl_display *display = wl_display_connect(NULL);
-  if (!display)
-    return 1;
+static void registry_handle_global(void *data, struct wl_registry *registry,
+                                   uint32_t name, const char *interface,
+                                   uint32_t version) {
+  printf("interface: '%s', version: '%d', name: '%d'\n", interface, version,
+         name);
+}
 
-  fprintf(stderr, "Connected!\n");
+static void registry_handle_global_remove(void *data,
+                                          struct wl_registry *registry,
+                                          uint32_t name) {}
+
+static const struct wl_registry_listener registry_listener = {
+    .global = registry_handle_global,
+    .global_remove = registry_handle_global_remove,
+};
+
+int main(int argc, char **argv) {
+  // Initialize display and registry
+
+  // display: wire protocol
+  // TYPE: request
+  // object ID: 1
+  // message length: 12 bytes
+  // opcode: 1
+  struct wl_display *display = wl_display_connect(NULL);
+  if (!display) {
+    fprintf(stderr, "Failed to create Wayland display.");
+    return 1;
+  }
+
+  // display: wire protocol
+  // TYPE: event
+  // object ID: 2
+  // message length: 13 bytes
+  // opcode: 0
+  struct wl_registry *registry = wl_display_get_registry(display);
+  if (!registry) {
+    fprintf(stderr, "Failed to create Wayland registry.");
+    return 1;
+  }
+  wl_registry_add_listener(registry, &registry_listener, NULL);
+  wl_display_roundtrip(display);
   return 0;
 }
--- src-bak/server.c
+++ src-bak/server.c
@@ -1,22 +1,124 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <wayland-server.h>
+
+// Group of globals are structured in a Linked List
+struct my_output {
+  struct wl_resource *resource;
+  struct my_state *state;
+  struct my_output *next;
+};
+
+struct my_state {
+  struct my_output *client_outputs;
+  int test;
+};
+
+// Helper functions for list management (e.g., add and remove nodes)
+void add_to_list(struct my_output **head, struct my_output *new_output) {
+  new_output->next = *head;
+  *head = new_output;
+}
+
+void remove_to_list(struct my_output **head, struct my_output *output) {
+  struct my_output **curr = head;
+  while (*curr) {
+    if (*curr == output) {
+      *curr = output->next;
+      free(output);
+      return;
+    }
+    curr = &(*curr)->next;
+  }
+}
+
+// Handles destruction of a Wayland resource
+static void wl_output_handle_resource_destroy(struct wl_resource *resource) {
+  struct my_output *client_output = wl_resource_get_user_data(resource);
+  if (!client_output)
+    return;
+
+  remove_to_list(&client_output->state->client_outputs, client_output);
+}
+
+// Called when client sends "release" request
+static void wl_output_handle_release(struct wl_client *client,
+                                     struct wl_resource *resource) {
+  wl_resource_destroy(resource);
+}
+
+// struct such that ^ is the destructor
+static const struct wl_output_interface wl_output_implementation = {
+    .release = wl_output_handle_release,
+};
+
+// Handles client requests and binds them to wl_output interface
+// @PARAM client -> client requesting to bind to wl_output interface
+// @PARAM data -> pointing to my_state struct
+// @PARAM version -> Wayland protocol version
+// @PARAM id -> For newly created resource
+// OUTPUT: Registers new resource as a global
+static void wl_output_handle_bind(struct wl_client *client, void *data,
+                                  uint32_t version, uint32_t id) {
+  // snapshot of compositor when wl_global_create() was called
+  struct my_state *state = data;
+
+  // Tracks client's interaction with wl_output
+  struct my_output *client_output = calloc(1, sizeof(struct my_output));
+  if (!client_output) {
+    fprintf(stderr, "Error: Failed to allocate memory for my_output.\n");
+    return;
+  }
+
+  // Initializes and adds resource to globals
+  struct wl_resource *resource =
+      wl_resource_create(client, &wl_output_interface, version, id);
+  if (!resource) {
+    fprintf(stderr, "Error: Failed to create wl_resource.\n");
+    free(client_output);
+    return;
+  }
+
+  wl_resource_set_implementation(resource, &wl_output_implementation,
+                                 client_output,
+                                 wl_output_handle_resource_destroy);
+
+  client_output->resource = resource;
+  client_output->state = state;
+
+  // TODO: Send geometry event, et al.
+
+  // wl_output_send_geometry(resource, 0, 0, 1920, 1080,
+  //                         WL_OUTPUT_SUBPIXEL_UNKNOWN, "Foobar, Inc.",
+  //                         "Foobar Monitor", WL_OUTPUT_TRANSFORM_NORMAL);
+
+  // Result: Another global that is a means of a client to interact with
+  // wl_display (global interface)
+  add_to_list(&state->client_outputs, client_output);
+}
 
 int main(int argc, char **argv) {
   struct wl_display *display = wl_display_create();
   if (!display) {
-    fprintf(stderr, "Unable to create Wayland display.");
-    return 1;
+    fprintf(stderr, "Error: Unable to create Wayland display.\n");
+    return EXIT_FAILURE;
   }
 
   const char *socket = wl_display_add_socket_auto(display);
   if (!socket) {
-    fprintf(stderr, "Unable to add socket to Wayland display.");
-    return 1;
+    fprintf(stderr, "Error: Unable to add socket to Wayland display.\n");
+    wl_display_destroy(display);
+    return EXIT_FAILURE;
   }
 
-  fprintf(stderr, "Running Wayland display on %s\n", socket);
+  struct my_state state = {.client_outputs = NULL, .test = 1};
+
+  wl_global_create(display, &wl_output_interface, 1, &state,
+                   wl_output_handle_bind);
+
+  fprintf(stderr, "Running Wayland display on socket: %s\n", socket);
   wl_display_run(display);
 
   wl_display_destroy(display);
-  return 0;
+  return EXIT_SUCCESS;
 }
